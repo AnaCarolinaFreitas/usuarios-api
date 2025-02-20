f3dea484506df842dc46e0698cef5e820441441f
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-02-20 14:19:42 -0300

    feat: edit server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const usersRoutes = require("./src/routes/usersRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', usersRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
